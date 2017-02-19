30c9dac14022bf52e6fbf38528780249b5a01b05
DESKTOP-82LUOQN\In-Kwon Park <inkwon112@koreatech.ac.kr>
2017-02-19 11:53:05 +0900

    코드주석/메뉴리뉴얼

--- app/src/main/java/com/example/in_kwonpark/qrcode/ReaderActivity.java
+++ app/src/main/java/com/example/in_kwonpark/qrcode/ReaderActivity.java
@@ -22,7 +22,7 @@ public class ReaderActivity extends AppCompatActivity {
 
     private DBHelper helper;
     private SQLiteDatabase db;
-    private Cursor c;
+    private Cursor c; // Cursor c 생성.(커서는 기본적으로 행(Row) 값을 참조 함)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +32,17 @@ public class ReaderActivity extends AppCompatActivity {
         final Activity activity = this;
 
         helper = new DBHelper(this);
-        // DBHelper 객체를 이용하여 DB 생성
+        // DBHelper 객체를 이용하여 DB 생성 -> DBHelper.java에 있는 onCreate() 자동 실행
         try {
-            db = helper.getWritableDatabase();
+            db = helper.getWritableDatabase(); // DB를 Write&Read 전용으로 연다.
             Toast.makeText(this,"Hello DB",Toast.LENGTH_SHORT).show(); // DB 연결 됨을 알림
-        } catch (SQLiteException e) {
-            db = helper.getReadableDatabase();
+        } catch (SQLiteException e) { // 데이터베이스를 열 수 없는 경우
+            db = helper.getReadableDatabase(); // Read전용으로 오픈
         }
 
-        c = db.rawQuery("SELECT * FROM ITEM", null);
+        c = db.rawQuery("SELECT * FROM ITEM", null); // item 테이블의 배열을 커서 c로 넘김
 
-        scan_btn.setOnClickListener(new View.OnClickListener() {
+        scan_btn.setOnClickListener(new View.OnClickListener() { // scan버튼 클릭 시(QR 코드)
             @Override
             public void onClick(View view) {
                 IntentIntegrator integrator = new IntentIntegrator(activity);
@@ -65,17 +65,18 @@ public class ReaderActivity extends AppCompatActivity {
             else{
                 Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
 
-                String Data = result.getContents();
+                String Data = result.getContents(); // QR코드로 부터 받은 컨텐츠를 Data 변수에 저장
 
-                String[] ItemValues = Data.split("/");
+                String[] ItemValues = Data.split("/"); // "/"단위로 ItemValues 배열에 순차적으로 저장
 
-                String Name = ItemValues[0];
-                String Start = ItemValues[1];
-                String Finish = ItemValues[2];
+                String Name = ItemValues[0]; // 상품명을 가리키는 Name
+                String Start = ItemValues[1]; // 제조일자를 가리키는 Start
+                String Finish = ItemValues[2]; // 유통기한을 가리키는 Finish
 
 
                 db.execSQL("insert into item values(null, '"+Name+"','"+Start+"','"+ Finish+"' );");
-                while(c.moveToNext()){
+
+                while(c.moveToNext()){ // item 테이블에 있는 항목을 모두 가져와 결과 값을 뽑아내는 while 문
                     Log.d("tag", "Table Search(index : "+c.getString(0)+" name : "+c.getString(1)+" start : "+c.getString(2)+" finish : "+c.getString(3)+")");
 
                 }
